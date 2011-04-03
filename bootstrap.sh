da28f5c86ca4fa047d5ad8d9db5e2d332a685105
Paul ADENOT <paul@paul.cx>
2011-04-03 14:01:05 +0200

    Added backup + symlink

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,13 +8,26 @@ install_font()
   wget paul.cx/$font -O ~/.fonts/$font
 }
 
+backup_old()
+{
+  mv -f ~/.vim ~/.vim.old
+  mv -f ~/.vimrc ~/.vimrc.old
+}
+
+backup_old
+
 # vim version
 [ -z $(vim --version | grep -o 7.3) ] && echo "You need ViM 7.3." && exit 1 
 echo "ViM version OK."
 
+# font
 locate $font && "DroidSansMono.ttf found." || install_font
 
+# ctags
 ctags && echo "ctags found" || sudo apt-get -y install ctags
 
+# fetch config
 cd ~ && git clone git://github.com/padenot/.vim && echo "Clone OK"
 
+# symlink .vimrc
+ln -s ~/.vim/vimrc ~/.vimrc
